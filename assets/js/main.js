e4b46d1e5ea5b7ebf3d1f757154acf9c04652885
mohamed72sa <83515792+mohamed72sa@users.noreply.github.com>
2023-07-07 22:35:13 +0300

    done 7/7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,50 @@
-let Name = document.getElementById('name')
-let email = document.getElementById('email')
-let password = document.getElementById('password')
-let roll = document.getElementById('roll')
 let btn = document.getElementById('btn').addEventListener('click', signup)
-
 var info = []
-
-let regex = {
+var regex = {
   name: /[a-z A-Z ]/,
   email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-  password: /[0-9]{8}/,
+  password: /[a-zA-Z0-9]{6,22}/,
   roll: /(admin|user)/,
 }
-function signup() {
+if (localStorage.getItem('information')) {
+  info = JSON.parse(localStorage.getItem('information'))
+}
 
+console.log(info)
+
+function signup() {
   let inputs = document.querySelectorAll('.inputup')
+  let Name = document.getElementById('name')
+  let email = document.getElementById('email')
+  let password = document.getElementById('password')
+  let roll = document.getElementById('roll')
   let error = valid(inputs, regex)
   if (!error) {
-    if (localStorage.getItem('information')) {
-        let data=JSON.parse(localStorage.getItem('information'))
-        data.forEach(user => {
-            if(user.email!=email.value){
-                let inf = {
-                    name: Name.value,
-                    email: email.value,
-                    password: password.value,
-                    roll: roll.value,
-                  }
-                  info.push(inf)
-                  localStorage.setItem('information', JSON.stringify(info))
-            }else{
-                email.nextElementSibling.textContent="asdasdasd"
-            }
-        });
+    if (localStorage.getItem('information')) {      
+        info.forEach((user) => {
+        if (email.value == user.email) {
+          email.nextElementSibling.textContent = 'asdasdasd'
+        } else {
+          let inf = {
+            name: Name.value,
+            email: email.value,
+            password: password.value,
+            roll: roll.value,
+          }
+          info.push(inf)
+          localStorage.setItem('information', JSON.stringify(info))
+        }
+      })
+    } else {
+      let inf = {
+        name: Name.value,
+        email: email.value,
+        password: password.value,
+        roll: roll.value,
+      }
+      info.push(inf)
+      localStorage.setItem('information', JSON.stringify(info))
     }
-   
   }
 }
 
@@ -52,12 +62,6 @@ function valid(inputs, regex) {
     } else {
       input.nextElementSibling.textContent = ''
     }
+  }
+  return error
 }
-return error;
-}
-//  if( regName.test(Name.value)==true &&regEmail.test(email.value)==true&& regpass.test(password.value)==ture&& regroll.test(roll.value)==true){
-//     return true
-// }
-// else{
-//     return false
-// }
