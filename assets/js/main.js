90376963d335c3d83e77b271e88dd98b0f355ed9
mohamed72sa <83515792+mohamed72sa@users.noreply.github.com>
2023-07-07 22:01:17 +0300

    code mohamed salah in valid function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,63 @@
-let inputs=document.querySelectorAll(".inputup");
-let Name=document.getElementById("name")
-let email=document.getElementById("email");
-let password=document.getElementById("password")
-let roll=document.getElementById("roll")
- let btn=document.getElementById("btn").addEventListener("submit",submit)
+let Name = document.getElementById('name')
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let roll = document.getElementById('roll')
+let btn = document.getElementById('btn').addEventListener('click', signup)
 
+var info = []
 
-
-var info= [];
-
-function submit(){
-
-    if (inputs != " " && reg() == true ){
-    let inf={
-    name:Name.value,
-    email:email.value,
-    password:password.value,
-    roll:roll.value
+let regex = {
+  name: /[a-z A-Z ]/,
+  email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  password: /[0-9]{8}/,
+  roll: /(admin|user)/,
 }
-     info.push(inf)
-    localStorage.setItem("information", JSON.stringify(info));
+function signup() {
 
-
- }
+  let inputs = document.querySelectorAll('.inputup')
+  let error = valid(inputs, regex)
+  if (!error) {
+    if (localStorage.getItem('information')) {
+        let data=JSON.parse(localStorage.getItem('information'))
+        data.forEach(user => {
+            if(user.email!=email.value){
+                let inf = {
+                    name: Name.value,
+                    email: email.value,
+                    password: password.value,
+                    roll: roll.value,
+                  }
+                  info.push(inf)
+                  localStorage.setItem('information', JSON.stringify(info))
+            }else{
+                email.nextElementSibling.textContent="asdasdasd"
+            }
+        });
+    }
+   
+  }
 }
- function reg(){
-    let regex=/[a-z A-Z ]/
-    let regex2=/^[a-z A-Z ][0-9][0-9]@ [gmail yahoo].com$/
-    let regex3=/[0-9]{8}/
-    let regex4=/(admin|user)/
-    if( regex.test(Name.value)==true &&regex2.test(email.value)==true&& regex3.test(password.value)==ture&& regex4.test(roll.value)==true){
-        return true
+
+function valid(inputs, regex) {
+  let error = false
+
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i]
+    if (input.value.length == 0) {
+      input.nextElementSibling.textContent = `${input.name} is empty`
+      error = true
+    } else if (!regex[input.name].test(input.value)) {
+      input.nextElementSibling.textContent = `${input.name} is not a valid`
+      error = true
+    } else {
+      input.nextElementSibling.textContent = ''
     }
-    else{
-        return false
-    }
- }
+}
+return error;
+}
+//  if( regName.test(Name.value)==true &&regEmail.test(email.value)==true&& regpass.test(password.value)==ture&& regroll.test(roll.value)==true){
+//     return true
+// }
+// else{
+//     return false
+// }
